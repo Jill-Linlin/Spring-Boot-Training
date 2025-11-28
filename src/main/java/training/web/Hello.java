458d5ad7ee934bb0d1f82f93f346aff8a03fe01d
Jill-Linlin <zirong0421@gmail.com>
2025-11-28 17:40:13 +0800

    homepage 建立

--- src/main/java/training/web/Hello.java
+++ src/main/java/training/web/Hello.java
@@ -1,8 +1,21 @@
 package training.web;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@RestController
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+        //本地127.0.0.1:8080
+        SpringApplication.run(Hello.class, args);
         
+    }
+    @GetMapping("/")//此專案的根目錄
+    public static String homepage(){
+        return "Hello World";
     }
     
 }
