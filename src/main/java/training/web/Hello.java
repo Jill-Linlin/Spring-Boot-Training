21bedfa98e8b8429b23680d2eeeae0afaedb80be
Jill-Linlin <zirong0421@gmail.com>
2025-11-29 21:14:25 +0800

    路由與路徑參數基礎

--- src/main/java/training/web/Hello.java
+++ src/main/java/training/web/Hello.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @SpringBootApplication
 @RestController
@@ -16,6 +17,18 @@ public class Hello {
     @GetMapping("/")//此專案的根目錄
     public static String homepage(){
         return "Hello World";
+    }
+    @GetMapping("/test")//此專案的test頁面
+    public static String testreply(){
+        return "This is TestReply page!";
+    }
+    @GetMapping("/user/{name}")//此專案的user頁面
+    public static String userreply(@PathVariable String name){
+        return "Hello this is the page by"+name;
+    }
+    @GetMapping("/square/{number}")//此專案的算術頁面
+    public static String resultreply(@PathVariable int number){
+        return "The result is"+(number+number);
     }
     
 }
