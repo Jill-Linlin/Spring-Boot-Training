dbd667069f3d14dbd428261d68aad541d09ba456
Jill-Linlin <zirong0421@gmail.com>
2025-11-30 19:21:23 +0800

    JSON 結構化回傳

--- src/main/java/training/web/Hello.java
+++ src/main/java/training/web/Hello.java
@@ -1,4 +1,7 @@
 package training.web;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -20,8 +23,10 @@ public class Hello {
         return "Hello World";
     }
     @GetMapping("/test")//此專案的test頁面
-    public static String testreply(){
-        return "This is TestReply page!";
+    public static List testreply(){
+        Point data1=new Point(4, 6);
+        Point data2=new Point(18, -5);
+        return List.of(data1,data2);
     }
     @GetMapping("/user/{name}")//此專案的user頁面
     public static String userreply(@PathVariable String name){
@@ -35,6 +40,16 @@ public class Hello {
     @GetMapping("/user")//此專案的user id查詢頁面
     public static String useridreply(@RequestParam int id){
         return "The page userId is "+id;
+    }
+    @GetMapping("/add")//此專案的user id查詢頁面
+    public static Map useridreply(@RequestParam int n1,@RequestParam int n2){
+        return Map.of("Ans",(n1+n2));
+    }
+    @GetMapping("/grades")//此專案的user id查詢頁面
+    public static Map gradesreply(){
+        int[] grades=new int[]{65,95,85,72,44};
+        String[] names=new String[]{"王小明","陳小美","張大強","許小莉","吳大天"};
+        return Map.of("Name: ",names,"Grade: ",grades);
     }
     
 }
---
+++ src/main/java/training/web/Point.java
@@ -0,0 +1,17 @@
+package training.web;
+
+public class Point {
+    public int x;
+    public int y;
+    public Point(int x,int y){
+        this.x=x;
+        this.y=y;
+    }
+    public int Getx(){
+        return this.x;
+    }
+    public int Gety(){
+        return this.y;
+    }
+    
+}
